05b0280220983b983c677876d4d5b286f1833ab7
Vincent Pinard <kavernn@gmail.com>
2021-02-25 16:11:20 -0500

    Correction du calcul de l'herbe du rhino

--- rhinoceros.cpp
+++ rhinoceros.cpp
@@ -1,5 +1,6 @@
 #include "rhinoceros.h"
 #include <iostream>
+#include <iomanip>
 
 Rhinoceros::Rhinoceros(std::string nom, float poids, int superficie) :Animal(nom, poids){
     if (superficie < 2000) {
@@ -15,7 +16,7 @@ Rhinoceros::Rhinoceros(std::string nom, float poids, int superficie) :Animal(nom
 	//Un rhinoceros peut recupérer 10puissance-2 kg/m2 d'herbe dans un enclos. Ce qui correspond à sa capacité d'ingestion. 
 	//La quantite d'herbe mangée est : quantite = capacité*superficie
 	float capacite = (float)1/100;
-	float herbeMangee = capacite*superficie;
+	float herbeMangee = capacite*this->superficie_enclos;
 	// Les besoins en herbe d'un rhinoceros correspondent à 2% du poids par jour
 	float besoin = ((float)2/100)*poids;
 	if (herbeMangee<besoin) {this->diete_rhinoceros.definirDieteHerbe(besoin-herbeMangee);}
@@ -28,7 +29,8 @@ Rhinoceros::Rhinoceros(std::string nom, float poids, int superficie) :Animal(nom
 void Rhinoceros::afficherInfos(){
     std::cout << "Rhinoceros : " << nom << " (" << poids << "kg)" <<std::endl;
     std::cout << "Enclos de " << superficie_enclos << " mètres-carrés" << std::endl;
-    std::cout << this->diete_rhinoceros.afficherDiete();
+    std::cout <<this->diete_rhinoceros.afficherDiete();
+    std::cout << "" << std::endl;
     
     
 };
